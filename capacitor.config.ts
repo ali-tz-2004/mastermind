6f915c6a1d4d98e6a1b10addec20600515ea5f54
alitz <alitz1382@gmail.com>
2026-09-23 17:57:27 +0330

    feat: add Android support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.ali.tz.mastermind",
+  appName: "فکر و بکر",
+  webDir: "build",
+};
+
+export default config;
